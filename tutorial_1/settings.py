1b7412e6bb134cc3dfa65cb769feb69f2281c3b0
prajwal <prajwal.parashkar@akronsystems.com>
2024-08-05 11:54:01 +0530

    added the docker compose for postgres and 2 new packages

--- tutorial_1/settings.py
+++ tutorial_1/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,13 +78,23 @@ WSGI_APPLICATION = 'tutorial_1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES ={
+    "default":{
+        "ENGINE":"django.db.backends.postgresql",
+       'NAME': os.getenv("POSTGRES_DB"),
+       'USER': os.getenv("POSTGRES_USER"),
+       'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+       'HOST': "localhost",
+       'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
